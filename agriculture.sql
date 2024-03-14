f5b634cc1f735c02e0a699602c39100d8a3e5cb0
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-03-14 20:55:51 +0530

    Update agriculture.sql

--- agriculture.sql
+++ agriculture.sql
@@ -3,101 +3,56 @@ SET time_zone = "+00:00";
 
 -- Table structure for table `blogdata`
 CREATE TABLE `blogdata` (
-  `blogId` int(10) NOT NULL AUTO_INCREMENT,
-  `blogUser` varchar(256) NOT NULL,
-  `blogTitle` varchar(256) NOT NULL,
-  `blogContent` longtext NOT NULL,
-  `blogTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `likes` int(10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`blogId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `blogId` int(10) NOT NULL AUTO_INCREMENT,`blogUser` varchar(256) NOT NULL,`blogTitle` varchar(256) NOT NULL,`blogContent` longtext NOT NULL,
+  `blogTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,`likes` int(10) NOT NULL DEFAULT '0',PRIMARY KEY (`blogId`)) ;
 
 -- Table structure for table `blogfeedback`
 CREATE TABLE `blogfeedback` (
-  `blogId` int(10) NOT NULL,
-  `comment` varchar(256) NOT NULL,
-  `commentUser` varchar(256) NOT NULL,
-  `commentPic` varchar(256) NOT NULL DEFAULT 'profile0.png',
-  `commentTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`blogId`),
-  FOREIGN KEY (`blogId`) REFERENCES `blogdata` (`blogId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `blogId` int(10) NOT NULL,`comment` varchar(256) NOT NULL,`commentUser` varchar(256) NOT NULL,`commentPic` varchar(256) NOT NULL DEFAULT 'profile0.png',
+  `commentTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`blogId`),FOREIGN KEY (`blogId`) REFERENCES `blogdata` (`blogId`));
 
 -- Table structure for table `buyer`
 CREATE TABLE `buyer` (
-  `bid` int(100) NOT NULL AUTO_INCREMENT,
-  `bname` varchar(100) NOT NULL,
-  `busername` varchar(100) NOT NULL,
-  `bpassword` varchar(100) NOT NULL,
-  `bhash` varchar(100) NOT NULL,
+  `bid` int(100) NOT NULL AUTO_INCREMENT,`bname` varchar(100) NOT NULL,`busername` varchar(100) NOT NULL,`bpassword` varchar(100) NOT NULL,`bhash` varchar(100) NOT NULL,
   `bemail` varchar(100) NOT NULL,
-  `bmobile` varchar(100) NOT NULL,
-  `baddress` text NOT NULL,
-  `bactive` int(100) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`bid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `bmobile` varchar(100) NOT NULL, `baddress` text NOT NULL,`bactive` int(100) NOT NULL DEFAULT '0',PRIMARY KEY (`bid`)) ;
 
 -- Table structure for table `farmer`
 CREATE TABLE `farmer` (
-  `fid` int(255) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(255) NOT NULL,
-  `fusername` varchar(255) NOT NULL,
-  `fpassword` varchar(255) NOT NULL,
-  `fhash` varchar(255) NOT NULL,
-  `femail` varchar(255) NOT NULL,
-  `fmobile` varchar(255) NOT NULL,
-  `faddress` text NOT NULL,
-  `factive` int(255) NOT NULL DEFAULT '0',
-  `frating` int(11) NOT NULL DEFAULT '0',
-  `picExt` varchar(255) NOT NULL DEFAULT 'png',
+  `fid` int(255) NOT NULL AUTO_INCREMENT,`fname` varchar(255) NOT NULL,`fusername` varchar(255) NOT NULL,`fpassword` varchar(255) NOT NULL,
+  `fhash` varchar(255) NOT NULL,`femail` varchar(255) NOT NULL,`fmobile` varchar(255) NOT NULL, `faddress` text NOT NULL,
+  `factive` int(255) NOT NULL DEFAULT '0',`frating` int(11) NOT NULL DEFAULT '0',`picExt` varchar(255) NOT NULL DEFAULT 'png',
   `picStatus` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`fid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 -- Table structure for table `fproduct`
 CREATE TABLE `fproduct` (
-  `fid` int(255) NOT NULL,
-  `pid` int(255) NOT NULL AUTO_INCREMENT,
-  `product` varchar(255) NOT NULL,
-  `pcat` varchar(255) NOT NULL,
-  `pinfo` varchar(255) NOT NULL,
-  `price` float NOT NULL,
-  `pimage` varchar(255) NOT NULL DEFAULT 'blank.png',
-  `picStatus` int(10) NOT NULL DEFAULT '0',
+  `fid` int(255) NOT NULL,`pid` int(255) NOT NULL AUTO_INCREMENT, `product` varchar(255) NOT NULL,`pcat` varchar(255) NOT NULL,
+  `pinfo` varchar(255) NOT NULL,`price` float NOT NULL,`pimage` varchar(255) NOT NULL DEFAULT 'blank.png',`picStatus` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ;
 
 -- Table structure for table `likedata`
 CREATE TABLE `likedata` (
-  `blogId` int(10) NOT NULL,
-  `blogUserId` int(10) NOT NULL,
+  `blogId` int(10) NOT NULL,`blogUserId` int(10) NOT NULL,
   FOREIGN KEY (`blogId`) REFERENCES `blogdata` (`blogId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ;
 
 -- Table structure for table `mycart`
 CREATE TABLE `mycart` (
-  `bid` int(10) NOT NULL,
-  `pid` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `bid` int(10) NOT NULL,`pid` int(10) NOT NULL
+) ;
 
 -- Table structure for table `review`
 CREATE TABLE `review` (
-  `pid` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `rating` int(10) NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `pid` int(10) NOT NULL,`name` varchar(255) NOT NULL,`rating` int(10) NOT NULL,`comment` text NOT NULL
+) ;
 
 -- Table structure for table `transaction`
 CREATE TABLE `transaction` (
-  `tid` int(10) NOT NULL AUTO_INCREMENT,
-  `bid` int(10) NOT NULL,
-  `pid` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `mobile` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `pincode` varchar(255) NOT NULL,
-  `addr` varchar(255) NOT NULL,
+  `tid` int(10) NOT NULL AUTO_INCREMENT,`bid` int(10) NOT NULL,`pid` int(10) NOT NULL,`name` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,`mobile` varchar(255) NOT NULL,`email` varchar(255) NOT NULL,
+  `pincode` varchar(255) NOT NULL,`addr` varchar(255) NOT NULL,
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) 
